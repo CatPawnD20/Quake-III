296b0517bf9fce53bb12ba292fe3d7c243d749dd
ataosman <ataosmanozguz@gmail.com>
2020-02-19 21:57:38 +0300

    MainActivity

---
+++ app/src/main/java/com/ab/quake_iii/OptionsActivity.java
@@ -0,0 +1,13 @@
+package com.ab.quake_iii;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OptionsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+    }
+}
